bd51f7ef876cd7001061caf8a44f19a2561501a7
sinhw0006 <sinhw0006@naver.com>
2025-02-13 14:46:41 +0900

    index modifiy

--- SRC/js/index.js
+++ SRC/js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-    let param = $(URLSearchParams)[0].location.search;
-    let page = param.replace('?', " ").split('=');
+    const URLSearchParam = new URLSearchParams(window.location.search);
+    const param = URLSearchParam.toString();
+
+    console.log(param);
+    let page = param.split('=');
     const html = (page[1] == undefined) ? "./main.html" : "./"+page[1] + ".html";
     $('header').load("./header.html");
     $('main').load(html);
--- SRC/js/match.js
+++ SRC/js/match.js
@@ -2,6 +2,9 @@ const matchInfoContianer = document.querySelector('.match-info-contianer');
 document.querySelector('.match-info-close').addEventListener('click',function(){
     matchInfoContianer.style.display = "none";
 });
-function matchInfoOpen(){
+document.querySelector('.match-content-btn').addEventListener('click',()=>{
     matchInfoContianer.style.display = "grid";
+})
+function matchInfoOpen(){
+    
 }
