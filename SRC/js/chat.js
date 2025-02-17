ce27c20d227b022c0d9a26735e1716df4ffade91
smj1249 <smj1249@gmail.com>
2025-02-17 10:40:53 +0900

    20250217_친구페이지수정22

---
+++ SRC/js/chat.js
@@ -0,0 +1,21 @@
+const mkchat = document.querySelector('.chat-pop-mkchat');
+document.querySelector('.chat-back-tab').addEventListener('click', function () {
+    mkchat.style.display = "none";
+});
+document.querySelectorAll('.chat-mk-chat').forEach((item) => {
+    item.addEventListener('click', () => {
+        mkchat.style.display = "block";
+    });
+});
+
+const mkfriend = document.querySelector('.chat-pop-mkfriend');
+document.querySelectorAll('.chat-back-tab').forEach((item) => {
+    item.addEventListener('click', () => {
+        mkfriend.style.display = "none";
+    });
+});
+document.querySelectorAll('.chat-mk-friend').forEach((item) => {
+    item.addEventListener('click', () => {
+        mkfriend.style.display = "block";
+    });
+});
