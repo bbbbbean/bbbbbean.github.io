0cbf2eb13da1c626b06f175d45696048bcacba65
sinhw0006 <sinhw0006@naver.com>
2025-02-19 14:42:20 +0900

    구조변경

--- static/js/index.js
+++ static/js/index.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 
     console.log(param);
     let page = param.split('=');
-    const html = (page[1] == undefined) ? "./main.html" : "./"+page[1] + ".html";
-    $('header').load("./header.html");
+    const html = (page[1] == undefined) ? "../pages/main/main.html" : "../pages/"+page[1] + ".html";
+    $('header').load("../pages/common/header.html");
     $('main').load(html);
-    $('footer').load("./footer.html");
+    $('footer').load("../pages/common/footer.html");
 });
