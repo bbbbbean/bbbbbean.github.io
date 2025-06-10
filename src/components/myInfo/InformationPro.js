5705625c1c230d872889b2ad583ac508fa852763
Leticia-134340 <why8575@naver.com>
2025-06-10 16:47:56 +0900

    친구 정보 틀

--- src/components/myInfo/InformationPro.js
+++ src/components/myInfo/InformationPro.js
@@ -8,12 +8,21 @@ const InformationPro = ({ userInfo }) => {
 
   console.log(userInfo);
   return (
-        <div className="info-container">
-            <div className="info-content">
-                <div className="info-header">
+        <div className="friendInfoContainer">
+            <div className="friendInfoContent">
+                <div className="friendInfoHeader">
                     <h2>유저 정보</h2>
                 </div>
-                <div className="info-list">
+                <div className="friendInfoList">
+                    <ul>
+                        <li><div>사진</div></li>
+                        <li>친구 닉네임</li>
+                        <li>친구 아이디</li> 
+                        <li>친구 소개</li> 
+                        <li>친구 이름 / 성별</li>
+                        <li>친구 생일</li>
+                        <li>친구 활동지역</li>
+                    </ul>
                 </div>
             </div>
         </div>
