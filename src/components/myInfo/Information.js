b5a779efaf787fc0d13c10a0397555bf01e4999c
Leticia-134340 <why8575@naver.com>
2025-06-10 15:03:09 +0900

    .

---
+++ src/components/myInfo/Information.js
@@ -0,0 +1,21 @@
+import api from "../../axios"
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import "../../css/common_css/information.css"
+
+const Information = ({ userInfo }) => {
+  //const { profile, nickName, name, userId, birthday, introduction, gender, address} = userInfo;
+
+  console.log(userInfo);
+  return (
+        <div className="info-container">
+            <div className="info-content">
+                <div className="info-header">
+                    <h2>유저 정보</h2>
+                </div>
+                <div className="info-list">
+                </div>
+            </div>
+        </div>
+)}
+export default Information;
