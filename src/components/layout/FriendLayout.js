c97b7998e0bb919d69336ebb20533e1c40c14579
Leticia-134340 <why8575@naver.com>
2025-06-10 15:09:44 +0900

    .

--- src/components/layout/FriendLayout.js
+++ src/components/layout/FriendLayout.js
@@ -1,7 +1,7 @@
 import '../../css/message_css/message.css';
 import FriendLeft from '../message/FriendLeft';
 import FriendRight from '../message/FriendRight';
-import Information from '../myInfo/Information';
+import InformationPro from '../myInfo/InformationPro';
 
 const MyPageLayout = () => {
 
@@ -10,7 +10,7 @@ const MyPageLayout = () => {
         <div className="friend-container">
             <FriendLeft />
             <FriendRight />
-            <Information />
+            <InformationPro />
         </div>
     );
 };
