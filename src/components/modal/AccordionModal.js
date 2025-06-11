4ac3876e735ee8840b6c28d67f6469718af40ae8
bin <ebbbbb515@gmail.com>
2025-06-11 09:26:38 +0900

    메인 아코디언 3D 효과 제거 테두리로 대체

--- src/components/modal/AccordionModal.js
+++ src/components/modal/AccordionModal.js
@@ -118,7 +118,7 @@ const AccordionModal = () => {
                   className={`que ${activeAccordion === item.index ? 'on' : ''}`}
                   onClick={(e) => handleAccordionClick(item.index, e)}
                 >
-                  <img src={item.icon} alt="아이콘" />
+                  <img src={item.icon} alt="아이콘" className="main-aco-icon"/>
                   <span>{item.title}</span>
                 </div>
 
