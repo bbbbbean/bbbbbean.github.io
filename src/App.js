d246cf4281d3f84ad70cf275c58fcc8d9700d0fa
Moon <experiment.wired@gmail.com>
2025-06-11 10:54:06 +0900

    공지사항 페이지 작성중

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import MainHeader from "./components/MainHeader";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import EventList from "./components/event/Event";
+import EventList from "./components/event/EventList";
 import MyPageLayout from "./components/layout/MypageLayout";
 import MatchLayout from "./components/layout/MatchLayout";
-import AdminPage from './AdminPage';
-import FriendLoyout from './components/layout/FriendLayout'
-import UserLayout from './components/layout/UserLayout'
-import SuccessLogin from './SuccessLogin'
-import AccordionModal from './components/modal/AccordionModal';
-import CommunityLayout from './components/layout/CommunityLayout';
-import { WebSocketProvider } from './WebSocket';
-import InformationPro from './components/myInfo/InformationPro'
-
+import AdminPage from "./AdminPage";
+import FriendLoyout from "./components/layout/FriendLayout";
+import UserLayout from "./components/layout/UserLayout";
+import SuccessLogin from "./SuccessLogin";
+import AccordionModal from "./components/modal/AccordionModal";
+import CommunityLayout from "./components/layout/CommunityLayout";
+import { WebSocketProvider } from "./WebSocket";
+import InformationPro from "./components/myInfo/InformationPro";
 
 function AppComponent() {
-
   //로그인 여부 확인
   const isAuth = localStorage.getItem("isAuth");
 
@@ -26,7 +24,7 @@ function AppComponent() {
   const location = useLocation().pathname;
   return (
     <WebSocketProvider>
-      {!location.includes('admin') ? (
+      {!location.includes("admin") ? (
         <>
           <div className="wrapper">
             <MainHeader />
@@ -44,20 +42,17 @@ function AppComponent() {
           <Footer />
           {isAuth && <AccordionModal />}
         </>
-      )
-        :
+      ) : (
         <Routes>
           <Route path="/admin/*" element={<AdminPage />} />
         </Routes>
-      }
+      )}
       <InformationPro />
-
     </WebSocketProvider>
   );
 }
 
 function App() {
-
   return (
     <Router>
       <AppComponent />
