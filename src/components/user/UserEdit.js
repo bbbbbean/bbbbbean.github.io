80872ef7e643f2c0a5954b9e1bf9691761700a9a
sinhw0006 <sinhw0006@naver.com>
2025-06-12 21:19:09 +0900

    ㅎㄷㄴㄱㅎㄷㄱㅎ

--- src/components/user/UserEdit.js
+++ src/components/user/UserEdit.js
@@ -201,7 +201,7 @@ const UserEditForm = ({ profile, setProfile }) => {
                 <span></span>
                 <div className="isPrivate">
                     <label>프로필 공개</label>
-                    <span>{userDTO.isPrivate ? "공개" : "비공개"}</span>
+                    <span>{userDTO.isPrivate === "0" ? "공개" : "비공개"}</span>
                     {editField !== "isPrivate" ? (
                         <button className="btn-edit my-page-btn" onClick={() => showEdit("isPrivate")}>수정하기</button>
                     ) : (
