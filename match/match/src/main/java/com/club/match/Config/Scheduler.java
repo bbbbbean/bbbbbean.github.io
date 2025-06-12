0e22d610b15ea299ed589ebdab16c4f735cbd032
sinhw0006 <sinhw0006@naver.com>
2025-06-12 23:29:55 +0900

    매칭 하루전 알람 1분 -> 1시간으로 설정(기본 값 : 1일마다)

--- match/match/src/main/java/com/club/match/Config/Scheduler.java
+++ match/match/src/main/java/com/club/match/Config/Scheduler.java
@@ -39,7 +39,7 @@ public class Scheduler {
         log.info("updateMatchStatus Count : "+ ok);
     }
 
-    @Scheduled(cron = "0 */1 * * * *")
+    @Scheduled(cron = "0 0 */1 * * *")
     public void alarm() {
 
         LocalDateTime now = LocalDateTime.now();
